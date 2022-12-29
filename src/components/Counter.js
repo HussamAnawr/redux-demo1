5eedf42bcb2f11b28ec979afca96e42bc91e3727
Hussam <o.hussam@gmail.com>
2022-12-29 21:36:25 +0300

    add dispatcher and selector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,29 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const isVisible = useSelector((state) => state.isVisible);
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {isVisible && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Dencrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,26 @@ import { createStore } from "redux";
 
 const INITIAL_COUNTER_STATE = {
   counter: 0,
+  isVisible: true,
 };
 
 const counterReducer = (state = INITIAL_COUNTER_STATE, action) => {
   if (action.type === "increment") {
     return {
+      ...state,
       counter: state.counter + 1,
     };
   }
   if (action.type === "decrement") {
     return {
+      ...state,
       counter: state.counter - 1,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      ...state,
+      isVisible: !state.isVisible,
     };
   }
   return INITIAL_COUNTER_STATE;
